ebd4ca6b2d57ea29e8cc162783f1387748f76a9a
arieltecay <arieltecay@gmail.com>
2022-06-23 11:28:49 -0300

    update clase

--- js/api.js
+++ js/api.js
@@ -5,23 +5,69 @@ class API {
         this.artista = artista;
         this.cancion = cancion;
     }
-    consultarAPI() {
+    async consultarAPI() {
         const url = `https://api.lyrics.ovh/v1/${this.artista}/${this.cancion}`;
         // Acá va el Spinner
-        UI.divResultado.innerHTML = "<div style='color:white;background:red'>Loading...</div>"
-        // METODO FETCH
-        fetch(url)
-            .then(respuesta => respuesta.json())
-            .then(resultado => {
-                // console.log(resultado);
-                const { lyrics } = resultado
+        UI.divResultado.innerHTML = '<div class="loader"></div>'
+        //Metodo ASYNC/AWAIT
+        try {
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
+            const { lyrics } = resultado
+            if (lyrics) {
                 UI.divResultado.textContent = lyrics
-            })
+                UI.headingResultado.textContent = `Cancion ${this.cancion.toUpperCase()} de ${this.artista.toUpperCase()}`
+            } else {
+                UI.divMensajes.textContent = 'Cancion no Encontrada'
+                UI.divMensajes.classList.add('error')
+                UI.divResultado.textContent = ' '
+                setTimeout(() => {
+                    UI.divMensajes.textContent = ' '
+                    UI.divMensajes.classList.remove('error')
+                }, 3000);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
     }
+
+
+    //Ver funcionamiento del try/catch ---> Especificamente del catch por culpa de Franco
+
+
+
+
+
+
+
+
+    // METODO FETCH
+    /* fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => {
+            // console.log(resultado);
+            const { lyrics } = resultado
+            if (lyrics) {
+                UI.divResultado.textContent = lyrics
+                UI.headingResultado.textContent = `Cancion ${this.cancion.toUpperCase()} de ${(this.artista).toUpperCase()}`
+            } else {
+                UI.divMensajes.textContent = 'Canción Inexistente'
+                UI.divMensajes.classList.add('error')
+                UI.divResultado.textContent = ''
+                setTimeout(() => {
+                    UI.divMensajes.textContent = ''
+                    UI.divMensajes.classList.remove('error')
+                }, 3000);
+            }
+        })
+} */
 }
 
-// Mostrar de mejor manera en el DOM y que al no cargar datos muestre los errores
-// Mejorar el spinner
+// Mostrar de mejor manera en el DOM y que al no cargar datos muestre los errores -----> Ok de 2 formas
+// Mejorar el spinner -----> OK
 // Utilizar async/await
+
+
 
 export default API
--- js/interfaz.js
+++ js/interfaz.js
@@ -5,4 +5,7 @@ export const formularioBuscar = document.querySelector('#formulario-buscar'),
     headingResultado = document.querySelector('.letra-resultado h2')
 
 
-    // Colocar las variables cancion y artista acá
+// Colocar las variables cancion y artista acá
+
+export const artista = document.querySelector('#artista')
+export const cancion = document.querySelector('#cancion')
